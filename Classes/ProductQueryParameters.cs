b372ac5387c2af21cb39ad76afc5fd6db2170ef7
Ahmad Jafari <71406620+sutedel@users.noreply.github.com>
2021-11-15 13:29:00 -0500

    Add project files.

---
+++ Classes/ProductQueryParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Classes
+{
+    public class ProductQueryParameters : QueryParameters
+    {
+        public string Sku { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/ShopContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options):base(options)
+        {  }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasMany(c => c.Products).WithOne(a => a.Category).HasForeignKey(a => a.CategoryId);
+            modelBuilder.Entity<Order>().HasMany(c => c.Products);
+            modelBuilder.Entity<Order>().HasOne(c => c.User);
+            modelBuilder.Entity<User>().HasMany(c => c.Orders).WithOne(a => a.User).HasForeignKey(a => a.UserId);
+            modelBuilder.Seed();
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category>Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet <User>Users { get; set; }
+
+    }
+}
